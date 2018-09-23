91b695424954a11c428843a215f3146b9f4d3252
Lucas Oliveira <lucas_oliveira_msn@hotmail.com>
2018-09-23 11:16:59 -0300

    teste

---
+++ Lista.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo_Dirigido_2
+{
+    class Lista
+    {
+        int cod;
+        string nome;
+        string status;
+
+        public void Imprime()
+        {
+            Console.WriteLine("{0}, {1}, {2}", cod, nome, status);
+        }
+
+        public Lista(int cod, string nome, string status)
+        {
+            this.cod = cod;
+            this.nome = nome;
+            this.status = status;
+        }
+    }
+}
---
+++ Produto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo_Dirigido_2
+{
+    class Produto
+    {
+        public int cod;
+        public string nome;
+        public string status;
+
+        public void Imprime()
+        {
+            Console.WriteLine("{0}, {1}, {2}",cod, nome, status);
+        }
+
+        public Produto(int cod, string nome, string status)
+        {
+            this.cod = cod;
+            this.nome = nome;
+            this.status = status;
+        }
+    }
+}
